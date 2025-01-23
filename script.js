9022ed0fdeb563c22cba2a85396bc29c5aed9bf1
abhishek <57187026+anonymous-cloud@users.noreply.github.com>
2025-01-23 17:50:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let counter = document.getElementById("counter")
-let btn = documment.getElementById("btn")
+let btn = document.getElementById("btn")
 
 let count = 0
 btn.addEventListner("click",()=>{
