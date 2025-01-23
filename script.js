3ed2be55dae3b0db884f3c3b3574cf27fdb9d586
abhishek <57187026+anonymous-cloud@users.noreply.github.com>
2025-01-23 17:48:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //your JS code here. If required.
-let p = document.getElementByid("p")
+let counter = document.getElementByid("counter")
 let btn = documment.getElementByid("btn")
 
-
+let count = 0
 btn.addEventListner("click",()=>{
-p.innerHtml += 	p.innerHtml
+count++
+counter.textElement = count;
+
+
+	
 })
