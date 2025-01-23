20b589f467f2c607067af62ab8a57f27fbf978f7
abhishek <57187026+anonymous-cloud@users.noreply.github.com>
2025-01-23 17:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let p = document.getElementByid("p")
+let btn = documment.getElementByid("btn")
+
+
+btn.addEventListner("click",()=>{
+p.innerHtml += 	p.innerHtml
+})
